c51d361b36626219167dc41b103beb1e683045a6
Quang Tran <quangshinoda1511@gmail.com>
2021-06-29 11:08:42 +0700

    print prime number smaller 100

--- java core/src/day1/PrimeNumberPrinting.java
+++ java core/src/day1/PrimeNumberPrinting.java
@@ -2,19 +2,16 @@ package day1;
 
 import java.util.Scanner;
 
-public class IsPrime {
+public class PrimeNumberPrinting {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int number = Integer.parseInt(sc.nextLine());
-
-        if (isPrime(number)){
-            System.out.println(number + " is a prime number");
-        } else {
-            System.out.println(number + " is not a prime number");
+        System.out.print("List of prime number smaller 100: ");
+        int i = 2;
+        while(i < 100){
+            if (isPrime(i)){
+                System.out.print(i + " ");
+            }
+            i++;
         }
-        
     }
 
     public static boolean isPrime(int number){
